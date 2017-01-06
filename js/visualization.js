589b8c9875694ba4bb88536518cf2370d830766c
Naram22 <naram_fr@hotmail.com>
2017-01-06 11:52:28 +0100

    ajout du zoom

--- js/visualization.js
+++ js/visualization.js
@@ -1,4 +1,6 @@
-var player={};
+//*******************************************//      
+ // Analyse des données	
+	var player={};
     var result={};
 	
 	var result_real ={};
@@ -172,10 +174,9 @@ var player={};
     //donnees2.sort(sortByRank);
     console.log(donnees2[0].result)
     
-    
-    
-    
-   
+  //******************************************************  
+  //******************************************************  
+  //******************************************************   
     
   // tracé du graph  
     
@@ -184,20 +185,9 @@ var player={};
          width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
     
-    //create the SVG container	
-    var svg = d3.select(".content").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-  
-    
-    //var groupe= svg.append("g")
-    //.attr("transform", "translate(20,20)");
-    
-    //var x = d3.scale.ordinal().rangeRoundBands([0, width]);
+    //Range of xaxis
     var x = d3.scale.ordinal().rangePoints([0, width]);
+    //x.domain(axis_x)
     x.domain(axis_x)
     var xAxis = d3.svg.axis().scale(x).orient("bottom");
     
@@ -210,6 +200,50 @@ var player={};
     var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
+    
+    var line = d3.svg.line()
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.y); });	
+    
+    // defines the function to call when zooming
+var zoom = d3.behavior.zoom()
+    //.x(x)
+    .y(y)
+    .on("zoom", zoomed);
+    
+    //create the SVG container	
+    var svg = d3.select(".content").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .call(zoom);
+  
+  svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "plot");
+    
+   // two functions uses for the zoom
+var make_x_axis = function () {
+    return d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(10);
+};
+
+var make_y_axis = function () {
+    return d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10);
+}; 
+    //var groupe= svg.append("g")
+    //.attr("transform", "translate(20,20)");
+    
+    //var x = d3.scale.ordinal().rangeRoundBands([0, width]);
+    
+    
     
     svg.append("g")
     .attr("class", "x axis")
@@ -226,46 +260,93 @@ var player={};
     .style("text-anchor", "end")
     .text("Rank diff");
     
-    //svg.selectAll("path")
-      // .data([donnees2])
-       //.enter()
- 			 //.append("path")
-       //.attr("class", "line")
-       //.attr("d", line);
-    //svg.selectAll("path")
-    //.data(donnees2)
-    //.enter()
-    //.append("path")
-    //.attr("class", "line")
-    //.attr("d", line);
+svg.append("g")
+    .attr("class", "x grid")
+    .attr("transform", "translate(0," + height + ")")
+    .call(make_x_axis()
+    .tickSize(-height, 0, 0)
+    .tickFormat(""));
+   
+svg.append("g")
+    .attr("class", "y grid")
+    .call(make_y_axis()
+    .tickSize(-width, 0, 0)
+    .tickFormat(""));
+
+    // objects for the zooming
+var clip = svg.append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height);
     
-    var line = d3.svg.line()
-    .x(function(d) { return x(d.x); })
-    .y(function(d) { return y(d.y); });	
+var chartBody = svg.append("g")
+    .attr("clip-path", "url(#clip)");    
     
-	for(var d=0;d<8;d++) {
-    svg.append("path")
+ //*********************************   
+ //plot the grap   
+    
+    for(var d=0;d<8;d++) {
+    //d=0;
+    chartBody.append("path")
     .datum(donnees2[d].reel)
     .attr("class", "line1")
     .attr("d", line);
     
-    svg.append("path")
+    chartBody.append("path")
     .datum(donnees2[d].prev)
     .attr("class", "line2")
     .attr("d", line);
     }
     
-    console.log(donnees2)
+    //console.log(donnees2)
+
     
     
+    //*****************************   
+ //define functions
     
-    //result=result.sort
-    //console.log(result)
-    //svg.append("path")
-    //.datum(donnees2)
-    //.attr("x", function(d) { return x(d.letter); })
-    //.attr("y", function(d) { return y(d.frequency); })
-    //.attr("class", "line")
+    // zooming functions
+function zoomed() {
+    //console.log(d3.event.translate);     // display information in the logging console of the browser (using developping tools)
+    //console.log(d3.event.scale);
+    svg.select(".x.axis").call(xAxis);
+    svg.select(".y.axis").call(yAxis);
+    svg.select(".x.grid")
+        .call(make_x_axis()
+        .tickSize(-height, 0, 0)
+        .tickFormat(""));
+    svg.select(".y.grid")
+        .call(make_y_axis()
+        .tickSize(-width, 0, 0)
+        .tickFormat(""));
+    chartBody.selectAll(".line1")
+        .attr("class", "line1")
+        .attr("d", line);
+   svg.selectAll(".line2")
+        .attr("class", "line2")
+        .attr("d", line);
+}
+    
+    function zoomed2() {
+  svg.select(".x.axis").call(xAxis);
+  svg.select(".y.axis").call(yAxis);
+}
+    d3.select("button").on("click", reset);
+
+    function reset() {
+  d3.transition().duration(1000).tween("zoom", function() {
+    var //ix = d3.interpolate(x.domain(), [-width / 2, width / 2]),
+        iy = d3.interpolate(y.domain(), [-300, 300]);
+    return function(t) {
+      zoom.y(y.domain(iy(t)));
+      zoomed();
+    };
+  });
+}
+    
     
 
    });
